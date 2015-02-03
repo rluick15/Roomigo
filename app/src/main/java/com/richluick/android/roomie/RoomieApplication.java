6b6619475e3bf7a7b8bc21e22ce7c6083c0be25a
Richard Luick <rluick15@gmail.com>
2015-02-03 11:24:20 -0500

    fb36

--- app/src/main/java/com/richluick/android/roomie/RoomieApplication.java
+++ app/src/main/java/com/richluick/android/roomie/RoomieApplication.java
@@ -3,9 +3,7 @@ package com.richluick.android.roomie;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
+import com.parse.ParseFacebookUtils;
 import com.richluick.android.roomie.utils.Constants;
 
 /**
@@ -15,23 +13,7 @@ public class RoomieApplication extends Application {
 
     public void onCreate() {
         Parse.initialize(this, Constants.APPLICATION_ID, Constants.CLIENT_KEY);
-
-        ParseUser user = new ParseUser();
-        user.setUsername("my name");
-        user.setPassword("my pass");
-        user.setEmail("email@example.com");
-
-// other fields can be set just like with ParseObject
-        user.put("phone", "650-555-0000");
-
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                } else {
-                }
-            }
-        });
+        ParseFacebookUtils.initialize(Constants.FACEBOOK_APP_ID);
     }
 
 }
