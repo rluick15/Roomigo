6aceb4d8280cb24a9bdf0ce148d1f63d8e74ab4c
Richard Luick <rluick15@gmail.com>
2015-02-03 16:12:30 -0500

    basic onboard layout with all views

--- app/src/main/java/com/richluick/android/roomie/ui/activities/OnBoardActivity.java
+++ app/src/main/java/com/richluick/android/roomie/ui/activities/OnBoardActivity.java
@@ -1,9 +1,7 @@
 package com.richluick.android.roomie.ui.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 import com.richluick.android.roomie.R;
 
@@ -13,28 +11,5 @@ public class OnBoardActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_on_board, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
