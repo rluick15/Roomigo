d939ce7d29e5c73d4abebe19a748f7c766e3ea20
Richard Luick <rluick15@gmail.com>
2015-02-03 14:46:17 -0500

    setup login on button click

--- app/src/main/java/com/richluick/android/roomie/ui/activities/LoginActivity.java
+++ app/src/main/java/com/richluick/android/roomie/ui/activities/LoginActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -19,17 +22,25 @@ public class LoginActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        ParseFacebookUtils.logIn(this, new LogInCallback() {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void done(ParseUser user, ParseException err) {
-                if (user == null) {
-                    Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
-                    Log.e("MyAppError", String.valueOf(err));
-                } else if (user.isNew()) {
-                    Log.d("MyApp", "User signed up and logged in through Facebook!");
-                } else {
-                    Log.d("MyApp", "User logged in through Facebook!");
-                }
+            public void onClick(View v) {
+                ParseFacebookUtils.logIn(LoginActivity.this, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user == null) {
+                            Log.d("MyApp", "Uh oh. The user cancelled the Facebook login.");
+                        } else if (user.isNew()) {
+                            Log.d("MyApp", "User signed up and logged in through Facebook!");
+                        } else {
+                            Log.d("MyApp", "User logged in through Facebook!");
+                        }
+                    }
+                });
             }
         });
     }
