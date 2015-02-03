29c00966954e72542327495a818689c6508ffae2
Richard Luick <rluick15@gmail.com>
2015-02-03 15:48:58 -0500

    intent flow for login screen

--- app/src/main/java/com/richluick/android/roomie/ui/activities/LoginActivity.java
+++ app/src/main/java/com/richluick/android/roomie/ui/activities/LoginActivity.java
@@ -1,19 +1,20 @@
 package com.richluick.android.roomie.ui.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
 import com.richluick.android.roomie.R;
-
+import com.richluick.android.roomie.utils.Constants;
 
 public class LoginActivity extends Activity {
 
@@ -22,9 +23,12 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         if(ParseUser.getCurrentUser().isAuthenticated()) {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
+            if(checkIfAlreadyOnBoarded()) {
+                mainIntent();
+            }
+            else {
+                onBoardIntent();
+            }
         }
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -38,15 +42,13 @@ public class LoginActivity extends Activity {
                     @Override
                     public void done(ParseUser user, ParseException e) {
                         if (user == null) {
-                            //Todo:handle login errors (maybe a toast??)
+                            //todo:handle sign in errors
                         }
                         else if (user.isNew()) {
-                            Toast.makeText(LoginActivity.this, "NEW USER!!", Toast.LENGTH_LONG).show();
+                            onBoardIntent();
                         }
                         else {
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(intent);
+                            mainIntent();
                         }
                     }
                 });
@@ -58,5 +60,28 @@ public class LoginActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
+    }
+
+    private Boolean checkIfAlreadyOnBoarded() {
+        SharedPreferences pref = getSharedPreferences(ParseUser.getCurrentUser().getUsername(),
+                Context.MODE_PRIVATE);
+        return pref.getBoolean(Constants.ALREADY_ONBOARD, false);
+
+//            SharedPreferences.Editor ed = pref.edit();
+//            ed.putBoolean("activity_executed", true);
+//            ed.commit();
+
+    }
+
+    private void onBoardIntent() {
+        Intent intent = new Intent(this, OnBoardActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void mainIntent() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 }
