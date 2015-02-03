546db9b4e0aa867d5d16827456cd99389a86531f
Richard Luick <rluick15@gmail.com>
2015-02-03 15:16:45 -0500

    clear task

--- app/src/main/java/com/richluick/android/roomie/ui/activities/LoginActivity.java
+++ app/src/main/java/com/richluick/android/roomie/ui/activities/LoginActivity.java
@@ -36,6 +36,7 @@ public class LoginActivity extends Activity {
                             Toast.makeText(LoginActivity.this, "NEW USER!!", Toast.LENGTH_LONG).show();
                         } else {
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                             startActivity(intent);
                         }
                     }
