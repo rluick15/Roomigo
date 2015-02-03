8f26408639473bdf38987f8cf7497a73fe6d9e44
Richard Luick <rluick15@gmail.com>
2015-02-03 17:40:02 -0500

    basic radio button layout

--- app/src/main/java/com/richluick/android/roomie/ui/activities/OnBoardActivity.java
+++ app/src/main/java/com/richluick/android/roomie/ui/activities/OnBoardActivity.java
@@ -1,15 +1,41 @@
 package com.richluick.android.roomie.ui.activities;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.view.Window;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.richluick.android.roomie.R;
 
-public class OnBoardActivity extends ActionBarActivity {
+public class OnBoardActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
+
+    private CheckBox mYesBox;
+    private CheckBox mNoBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_on_board);
+
+//        mYesBox = (CheckBox) findViewById(R.id.yesCheckBox);
+//        mNoBox = (CheckBox) findViewById(R.id.noCheckBox);
+//
+//        mYesBox.setOnCheckedChangeListener(this);
+//        mNoBox.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()) {
+            case R.id.yesCheckBox:
+                mNoBox.setChecked(false);
+                break;
+            case R.id.noCheckBox:
+                mYesBox.setChecked(false);
+                mNoBox.setChecked(true);
+                break;
+        }
     }
 }
