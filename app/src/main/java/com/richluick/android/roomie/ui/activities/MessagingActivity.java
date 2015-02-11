ed8571b2b2fe6a91162c19f289b18f71a72ab7d1
Richard Luick <rluick15@gmail.com>
2015-02-11 14:58:31 -0500

    basic message activity_messaging layout

---
+++ app/src/main/java/com/richluick/android/roomie/ui/activities/MessagingActivity.java
@@ -0,0 +1,74 @@
+package com.richluick.android.roomie.ui.activities;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
+import com.richluick.android.roomie.R;
+import com.richluick.android.roomie.utils.MessageService;
+
+public class MessagingActivity extends ActionBarActivity {
+
+    private String recipientId;
+    private EditText messageBodyField;
+    private String messageBody;
+    private MessageService.MessageServiceInterface messageService;
+    private String currentUserId;
+    private ServiceConnection serviceConnection = new MyServiceConnection();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+
+        bindService(new Intent(this, MessageService.class), serviceConnection, BIND_AUTO_CREATE);
+
+        //get recipientId from the intent
+        Intent intent = getIntent();
+        recipientId = intent.getStringExtra("RECIPIENT_ID");
+        currentUserId = ParseUser.getCurrentUser().getObjectId();
+
+        messageBodyField = (EditText) findViewById(R.id.messageBodyField);
+
+        //listen for a click on the send button
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                messageBody = messageBodyField.getText().toString();
+                if (messageBody.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter a message", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                messageService.sendMessage(recipientId, messageBody);
+                messageBodyField.setText("");
+            }
+        });
+    }
+
+    //unbind the service when the activity is destroyed
+    @Override
+    public void onDestroy() {
+        unbindService(serviceConnection);
+        super.onDestroy();
+    }
+
+    private class MyServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            messageService = (MessageService.MessageServiceInterface) iBinder;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            messageService = null;
+        }
+    }
+}
