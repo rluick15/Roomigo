07d8a2a7111581ebb7c8133ba6f8c59527146c43
Richard Luick <rluick15@gmail.com>
2015-02-12 10:34:12 -0500

    update Application class and send first push

--- app/src/main/java/com/richluick/android/roomie/RoomieApplication.java
+++ app/src/main/java/com/richluick/android/roomie/RoomieApplication.java
@@ -1,9 +1,13 @@
 package com.richluick.android.roomie;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 import com.richluick.android.roomie.utils.Constants;
 
 /**
@@ -14,6 +18,17 @@ public class RoomieApplication extends Application {
     public void onCreate() {
         Parse.initialize(this, Constants.APPLICATION_ID, Constants.CLIENT_KEY);
         ParseFacebookUtils.initialize(Constants.FACEBOOK_APP_ID);
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 
 }
