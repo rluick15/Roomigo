0343dda7c27e5e526f2202f705f2f975daba70c7
Richard Luick <rluick15@gmail.com>
2015-02-20 12:53:56 -0500

    refactor intents into utils

--- app/src/main/java/com/richluick/android/roomie/ui/activities/LauncherActivity.java
+++ app/src/main/java/com/richluick/android/roomie/ui/activities/LauncherActivity.java
@@ -11,10 +11,7 @@ import android.widget.TextView;
 
 import com.parse.ParseUser;
 import com.richluick.android.roomie.R;
-import com.richluick.android.roomie.RoomieApplication;
-import com.richluick.android.roomie.facebook.FacebookRequest;
-import com.richluick.android.roomie.utils.Constants;
-import com.richluick.android.roomie.utils.MessageService;
+import com.richluick.android.roomie.utils.IntentUtils;
 
 public class LauncherActivity extends Activity {
 
@@ -35,10 +32,10 @@ public class LauncherActivity extends Activity {
             public void run() {
                 if(ParseUser.getCurrentUser() != null) {
                     if (ParseUser.getCurrentUser().isAuthenticated()) {
-                        if (checkIfAlreadyOnBoarded()) {
-                            mainIntent();
+                        if (IntentUtils.checkIfAlreadyOnBoarded()) {
+                            IntentUtils.mainIntent(LauncherActivity.this);
                         } else {
-                            onBoardIntent();
+                            IntentUtils.onBoardIntent(LauncherActivity.this);
                         }
                     }
                 }
@@ -49,49 +46,5 @@ public class LauncherActivity extends Activity {
                 }
             }
         }, 2000);
-    }
-
-    /**
-     * This method  checks if the user has already gone through the onboard process and either
-     * sends them to onboarding if they have not or skips that step and brings them to the
-     * Main Activity
-     *
-     * @return Boolean true or false depending on if the user has onboarded or not
-     */
-    private Boolean checkIfAlreadyOnBoarded() {
-        return (Boolean) ParseUser.getCurrentUser().get(Constants.ALREADY_ONBOARD);
-    }
-
-    /**
-     * This method  sends the user to onboarding using an intent and also set the current facebook
-     * user id in the shared preferences.
-     */
-    private void onBoardIntent() {
-        new FacebookRequest(this).setCurrentFacebookUser(); //sets the user to shared prefs
-
-        final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
-        startService(serviceIntent);
-
-        RoomieApplication.updateParseInstallation();
-
-        Intent intent = new Intent(this, OnBoardActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
-
-    /**
-     * This method  sends the user to the main activity using an intent
-     */
-    private void mainIntent() {
-        new FacebookRequest(this).setCurrentFacebookUser(); //sets the user to shared prefs
-
-        final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
-        startService(serviceIntent);
-
-        RoomieApplication.updateParseInstallation();
-
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
     }
 }
